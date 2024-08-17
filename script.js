83efb43a40927ee339fb87a20906f9aae5ab64bf
Anamika <anamikadobriyal21@gmail.com>
2024-08-17 21:36:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,77 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
+	
+    let totalCost = 0;
+    const minHeap = new MinHeap();
+
+    arr.forEach(length => minHeap.insert(length));
+
+    while (minHeap.size() > 1) {
+        const first = minHeap.extractMin();
+        const second = minHeap.extractMin();
+        const cost = first + second;
+        totalCost += cost;
+        minHeap.insert(cost);
+    }
+
+    return totalCost;
+}
+
+class MinHeap {
+    constructor() {
+        this.heap = [];
+    }
+
+    insert(val) {
+        this.heap.push(val);
+        this.bubbleUp();
+    }
+
+    extractMin() {
+        if (this.heap.length === 0) return null;
+        if (this.heap.length === 1) return this.heap.pop();
+        const min = this.heap[0];
+        this.heap[0] = this.heap.pop();
+        this.bubbleDown();
+        return min;
+    }
+
+    bubbleUp() {
+        let index = this.heap.length - 1;
+        while (index > 0) {
+            const parentIndex = Math.floor((index - 1) / 2);
+            if (this.heap[index] >= this.heap[parentIndex]) break;
+            [this.heap[index], this.heap[parentIndex]] = [this.heap[parentIndex], this.heap[index]];
+            index = parentIndex;
+        }
+    }
+
+    bubbleDown() {
+        let index = 0;
+        const length = this.heap.length;
+        while (true) {
+            const leftChildIndex = 2 * index + 1;
+            const rightChildIndex = 2 * index + 2;
+            let smallestIndex = index;
+
+            if (leftChildIndex < length && this.heap[leftChildIndex] < this.heap[smallestIndex]) {
+                smallestIndex = leftChildIndex;
+            }
+            if (rightChildIndex < length && this.heap[rightChildIndex] < this.heap[smallestIndex]) {
+                smallestIndex = rightChildIndex;
+            }
+            if (smallestIndex === index) break;
+            [this.heap[index], this.heap[smallestIndex]] = [this.heap[smallestIndex], this.heap[index]];
+            index = smallestIndex;
+        }
+    }
+
+    size() {
+        return this.heap.length;
+    }
+
+
   
 }
 
